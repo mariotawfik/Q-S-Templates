f5f38b58cc49c87812310eaaf25e41fa6c8c0f53
mariotawfik <amxmaroz@gmail.com>
2022-04-10 19:25:39 +0200

    Initial Commit

---
+++ Q&S Templates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Q&S Templates
+//
+//  Created by Mary Tamry on 10.04.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
